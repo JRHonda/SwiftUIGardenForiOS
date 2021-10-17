e1b7c01d59a40196d6c1164b4c2d650f5e4ef8ab
Justin Honda <justinohonda@gmail.com>
2021-10-16 21:06:27 -0400

    Initial Commit

---
+++ SwiftUIGardenForiOS/SwiftUIGardenForiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGardenForiOSApp.swift
+//  SwiftUIGardenForiOS
+//
+//  Created by Justin Honda on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGardenForiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
